b6ca9c956858da14d79e77199f6ab97d90f95d5e
Sascha Andeweg <sascha@designed.irish>
2020-01-18 23:00:27 +0000

    commit of the FizzBuzz challenge so far at 11 pm

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Write a set of tests that pass in various values to the FizzBuzz function and en
 
 The Jasmine framework is used and the files fizzBuzzSpec.js and fizzBuzz.js will be run using the boilerplate in index.html.
 
-The test will be done on calling the function on "30", "9", "20", "17" and a string "fifteen".
+The test will be done on calling the function on "30", "9", "20" and "17".
--- specs/fizzBuzzSpec.js
+++ specs/fizzBuzzSpec.js
@@ -1,12 +1,30 @@
-descibe("FizzBuzz tests", function () {
+
+describe("FizzBuzz test function", function () {
+    var fb;
+    
     beforeEach(function() {
         fb = new fizzBuzz();
     });
 
-    describe("check various divisions in FizzBuzz", function() {
+    describe("Checks various divisions and what is returned", function() {
+        it("should exist", function() {
+            expect(fizzBuzz).toBeDefined();
+        });
         it("should return FizzBuzz when called fizzBuzz(30)", function() {
             var result = fizzBuzz(30)
             expect(result).toBe("FizzBuzz");
         });
-    });
-})
+        it("should return Fizz when called fizzBuzz(9)", function() {
+            var result = fizzBuzz(9)
+            expect(result).toBe("Fizz");
+        });
+        it("should return Buzz when called fizzBuzz(20)", function() {
+            var result = fizzBuzz(20)
+            expect(result).toBe("Buzz");
+        });
+        it("should return 17 when called fizzBuzz(17)", function() {
+            var result = fizzBuzz(17)
+            expect(result).toBe(17);
+        });
+   });
+});
