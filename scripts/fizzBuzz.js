41e1a4b5fce03694c9c0a1541970a5741157ea9a
Sascha Andeweg <sascha@designed.irish>
2020-01-19 14:40:05 +0000

    refactored js file to using number a a parameter and make it pass the jasmine test

--- scripts/fizzBuzz.js
+++ scripts/fizzBuzz.js
@@ -1,14 +1,14 @@
-fizzBuzz = function(i) {
-    for (i=1;i<101;i++) {
-        if (i % 15 === 0) {
-            return "FizzBuzz";
-        } else if (i % 3 === 0) {
-            return "Fizz";
-        } else if (i % 5 === 0) {
-            return "Buzz";
-        }
-        else {
-            return(i);
-        }
-    };
-}
+fizzBuzz = function(number) {
+	if (number % 3 == 0 && number % 5 == 0) {
+		return "FizzBuzz";
+	}
+	else if (number % 3 == 0) {
+		return "Fizz";
+	}
+	else if (number % 5 == 0) {
+		return "Buzz";
+	}	
+	else {
+		return number
+	}
+};
